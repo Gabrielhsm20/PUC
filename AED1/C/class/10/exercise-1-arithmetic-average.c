7bda95a13a9a9b486ac2fa45f944f741dac9363c
Gabriel Henrique <gabrielhsm20@gmail.com>
2020-03-19 18:36:24 -0300

    .

---
+++ AED1/C/class/10/exercise-1-arithmetic-average.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    float average = 0;
+
+    for(int i = 0; i < 10; i++){
+        float score;
+
+        printf("Insira a %iª nota (Ex: 10): ", i+1);
+        scanf("%f", &score);
+        fflush(stdin);
+
+        average += score > 0 ? score : 0;
+    }
+    
+    printf("A média aritimética das notas é %f", average/10);
+
+    return 0;
+}
---
+++ AED1/C/class/10/exercise-2-sort-numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    float higher = 0, lowest = 0;
+
+    for(int i = 0; i < 5; i++){
+        float num;
+
+        printf("Insira o %iº número (Ex: 10): ", i+1);
+        scanf("%f", &num);
+        fflush(stdin);
+
+        higher = i == 0 ? num : num > higher ? num : higher;
+        lowest = i == 0 ? num : num < lowest ? num : lowest;
+    }
+    
+    printf("O maior número é: %f\nO menor número é: %f", higher, lowest);
+
+    return 0;
+}
