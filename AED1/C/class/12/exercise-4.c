90bf0f33bfcfe6a7318ac6adf912f78ab7b47526
Gabriel Henrique <gabrielhsm20@gmail.com>
2020-04-06 22:24:38 -0300

    .

--- AED1/C/class/12/exercise-4.c
+++ AED1/C/class/12/exercise-4.c
@@ -1,34 +1,29 @@
 #include <stdio.h>
 
-void smallestNumber(int *num1, int *num2, int *num3, int *num4, int *num5, int *smaller, int *address){
+int * numberAddress(int *num1, int *num2, int *num3, int *num4, int *num5){
     if(*num1 <= *num2 && *num1 <= *num3 && *num1 <= *num4 && *num1 <= *num5){
-        *address = &num1;
-        *smaller = *num1;
+        return num1;
     }else if(*num2 <= *num1 && *num2 <= *num3 && *num2 <= *num4 && *num2 <= *num5){
-        *address = &num2;
-        *smaller = *num2;
+        return num2;
     }else if(*num3 <= *num1 && *num3 <= *num2 && *num3 <= *num4 && *num3 <= *num5){
-        *address = &num3;
-        *smaller = *num3;
+        return num3;
     }else if(*num4 <= *num1 && *num4 <= *num2 && *num4 <= *num3 && *num4 <= *num5){
-        *address = &num4;
-        *smaller = *num4;
+        return num4;
     }else{
-        *address = &num5;
-        *smaller = *num5;
+        return num5;
     }
 }
 
 int main() {
-    int num1, num2, num3, num4, num5, smaller, address;
+    int num1, num2, num3, num4, num5;
 
     printf("Insira 5 numeros inteiros para visualizar o endereco na memoria de suas variaveis (Ex: 1 2 3 4 5): ");
     scanf("%i %i %i %i %i", &num1, &num2, &num3, &num4, &num5);
     fflush(stdin);
 
-    smallestNumber(&num1, &num2, &num3, &num4, &num5, &smaller, &address);
+    int *address = numberAddress(&num1, &num2, &num3, &num4, &num5);
 
-    printf("O menor numero e %i e seu endereco e %p", smaller, address);
+    printf("O menor numero e %i e seu endereco e %p", *address, address);
 
     return 0;
 }
